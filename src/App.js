0eb35e5486ed809eaba400ec64a0dabb4d0f4d73
naser23 <naserabdulkadir0@gmail.com>
2022-03-01 14:56:30 -0500

    started working on navbar.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./layout/Home";
-import DesktopBg from "./assets/home/background-home-desktop.jpg";
-import MobileBg from "./assets/home/background-home-mobile.jpg";
-import TabletBg from "./assets/home/background-home-tablet.jpg";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="w-screen h-screen bg-no-repeat bg-cover sm: bg-mobile-bg md: bg-tablet-bg lg:bg-desktop-bg xl:bg-desktop-bg"></div>
+    <div className="w-screen h-screen bg-no-repeat bg-cover sm: bg-mobile-bg md: bg-tablet-bg lg:bg-desktop-bg xl:bg-desktop-bg">
+      <Navbar />
+    </div>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Navbar() {
+  return <div className="bg-white h-16 md:h-16 lg:h-1/6">Navbar</div>;
+}
+
+export default Navbar;
