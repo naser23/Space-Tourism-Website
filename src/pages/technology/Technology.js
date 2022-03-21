533ab5758f120411557a3556843f16e30e75e90b
naser23 <naserabdulkadir0@gmail.com>
2022-03-21 16:21:50 -0400

    starting on the technology page.

--- src/pages/technology/Technology.js
+++ src/pages/technology/Technology.js
@@ -1,11 +1,33 @@
 import React from "react";
 import Navbar from "../../components/layout/navbar/Navbar";
+import { useState, useContext } from "react";
+import NavbarContext from "../../context/NavbarContext";
+import LaunchImg from "../../assets/technology/image-launch-vehicle-landscape.jpg";
+import CapsuleImg from "../../assets/technology/image-space-capsule-landscape.jpg";
 
 function Technology() {
+  const { data } = useContext(NavbarContext);
+  const technologyData = data.technology;
+  console.log(technologyData);
+
   return (
-    <div className="text-white w-screen h-screen sm:h-full md:h-screen lg:h-screen bg-no-repeat bg-cover sm: bg-technologyMobile md:bg-technologyTablet lg:bg-technologyDesktop xl:bg-technologyDesktop">
+    <div className="technology-container">
       <Navbar />
-      Technology
+      <main className="technology-content-container">
+        <h1 className="crew-display-header">
+          <span className="gray-text">03</span> SPACE LAUNCH 101
+        </h1>
+        <img
+          className="technology-hero-img"
+          src={LaunchImg}
+          alt="technology img"
+        />
+        <section className="technology-ux">
+          <nav className="technology-navigation">
+            <ul></ul>
+          </nav>
+        </section>
+      </main>
     </div>
   );
 }
