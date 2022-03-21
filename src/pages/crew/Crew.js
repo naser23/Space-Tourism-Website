4760876c802b9681d28464cc3eff7f6300563aac
naser23 <naserabdulkadir0@gmail.com>
2022-03-21 11:40:23 -0400

    almost done with the crew page. Just have to make the active link change color on the crew-member-navigation page and after that I might go back and refactor the styling.

--- src/pages/crew/Crew.js
+++ src/pages/crew/Crew.js
@@ -1,13 +1,24 @@
 import React from "react";
 import Navbar from "../../components/layout/navbar/Navbar";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import NavbarContext from "../../context/NavbarContext";
 import DouglasImg from "../../assets/crew/image-douglas-hurley.png";
+import MarkImg from "../../assets/crew/image-mark-shuttleworth.png";
+import VictorImg from "../../assets/crew/image-victor-glover.png";
+import AnoushehImg from "../../assets/crew/image-anousheh-ansari.png";
 
 function Crew() {
   const { data } = useContext(NavbarContext);
   const crewData = data.crew;
   console.log(crewData);
+
+  const [index, setIndex] = useState(0);
+
+  function updateIndex(key) {
+    setIndex(key);
+  }
+
+  const crewPictures = [DouglasImg, MarkImg, VictorImg, AnoushehImg];
 
   return (
     <div className="crew-container">
@@ -16,21 +27,28 @@ function Crew() {
         <h1 className="crew-display-header">
           <span className="gray-text">02</span> MEET YOUR CREW
         </h1>
-        <img className="crew-member" src={DouglasImg} alt="crew-img" />
+        <img className="crew-member" src={crewPictures[index]} alt="crew-img" />
         <div className="crew-divider"></div>
         <div className="crew-ux">
           <nav className="crew-member-navigation">
             <ul>
+              {/* <li></li>
               <li></li>
               <li></li>
-              <li></li>
-              <li></li>
+              <li></li> */}
+              {crewData &&
+                crewData.map((member) => (
+                  <li
+                    key={crewData.indexOf(member)}
+                    onClick={() => updateIndex(crewData.indexOf(member))}
+                  ></li>
+                ))}
             </ul>
           </nav>
           <section className="crew-member-info">
-            <h2 className="member-role">{crewData && crewData[0].role}</h2>
-            <h1 className="member-name">{crewData && crewData[0].name}</h1>
-            <p className="member-bio">{crewData && crewData[0].bio}</p>
+            <h2 className="member-role">{crewData && crewData[index].role}</h2>
+            <h1 className="member-name">{crewData && crewData[index].name}</h1>
+            <p className="member-bio">{crewData && crewData[index].bio}</p>
           </section>
         </div>
       </main>
